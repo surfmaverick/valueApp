06c455fad936032ee2a02a1565cec6cf972b2480
surfmaverick <smith_c@icloud.com>
2014-05-11 14:27:57 -0400

    updated for produciton schedule variation

--- main.py
+++ main.py
@@ -7,29 +7,30 @@ from datetime import datetime
 import pandas as pd
 
 
-#constants are declared here. To be moved into a user configuration file or front end later.
 
-prodschedule = {12: 34450, 11: 23424, 10: 22424, 9: 24343, 8: 23423, 7: 11433, 6: 23432, 5: 23535, 4: 19191, 3: 23432,
+prodschedulemonthly = {12: 34450, 11: 23424, 10: 22424, 9: 24343, 8: 23423, 7: 11433, 6: 23432, 5: 23535, 4: 19191, 3: 23432,
     2: 23423, 1: 23423}
 
+prodschedulequarterly = asset.monthlytoquarterly(prodschedulemonthly,firstquarterendingmonth=3)
 
 
 
 
 # instantiate the asset
-test = asset.WindPowerPlant(name='test')
+test = asset.WindPowerPlant(name='test',periods='Q',type='OffshoreWind')
 
 
 # first we must must properly setup the dates that we will.  This is a critiacl first step as the date ranges will drive
 # the balance of the programming and modeling required
 
-#first setup the frame for operations
-dates = preparedates(datetime(2015, 1, 15), datetime(2017, 1, 27), 'M')
-test.setupframe(dates, dfname='ops')
-
 #then setup the frame for construction
-dates = preparedates(datetime(2015, 1, 27), datetime(2035, 1, 26), 'M')
+dates = preparedates(startdate=datetime(2015, 1, 27), enddate=datetime(2017, 1, 27), freq='M')
 test.setupframe(dates, dfname='construction')
 
+#first setup the frame for operations
+dates = preparedates(startdate=datetime(2017, 1, 27), enddate=datetime(2035, 1, 27), freq='Q')
+test.setupframe(dates, dfname='ops')
 
-test.setupproductionschedule(prodschedule, dfname='ops', dfcolumn='production')
+test.setupproductionschedule(prodschedulequarterly, dfname='ops', dfcolumn='production')
+
+dates = preparedates(startdate=datetime(2017, 1, 27), enddate=datetime(2035, 1, 27), freq='Q')
